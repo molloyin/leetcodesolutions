4b2b27832d1d173616264e36c6850dae2863a5af
Matthew Molloy <83689572+molloyin@users.noreply.github.com>
2023-01-23 08:55:22 +1300

    Update fizzbuzz.cpp

--- easy/fizzbuzz.cpp
+++ easy/fizzbuzz.cpp
@@ -1,31 +1,16 @@
 // I've pwned fizzbuzz... officially a genius!! :D ( top 1% of runtime, but only beats 39% of memory usage...)
-class Solution
-{
+class Solution {
 public:
-    vector<string> fizzBuzz(int n)
-    {
+    vector<string> fizzBuzz(int n) {
         int fizz_cond = 3;
         int buzz_cond = 5;
         vector<string> fizz_buzz_output;
-        for (int i = 1; i <= n; i++)
-        {
-            if (i % fizz_cond == 0 && i % buzz_cond == 0)
-            {
-                fizz_buzz_output.push_back("FizzBuzz");
-            }
-            else if (i % fizz_cond == 0)
-            {
-                fizz_buzz_output.push_back("Fizz");
-            }
-            else if (i % buzz_cond == 0)
-            {
-                fizz_buzz_output.push_back("Buzz");
-            }
-            else
-            {
-                fizz_buzz_output.push_back(to_string(i));
-            }
-        }
+        for (int i = 1; i <= n; i++) {
+            if (i % fizz_cond == 0 && i % buzz_cond == 0)   { fizz_buzz_output.push_back("FizzBuzz"); }
+            else if (i % fizz_cond == 0)                    { fizz_buzz_output.push_back("Fizz"); }
+            else if (i % buzz_cond == 0)                    { fizz_buzz_output.push_back("Buzz"); }
+            else                                            { fizz_buzz_output.push_back(to_string(i)); }
+        } 
 
         return fizz_buzz_output;
     }
