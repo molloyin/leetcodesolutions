b59b3f75433956adb82af31030b0a4e69a171ae7
Matthew Molloy <83689572+molloyin@users.noreply.github.com>
2023-07-03 22:04:36 +1200

    Hashmap frequency count, sort and convert to tuple

---
+++ medium/topkfreqs.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        # store freqs of each element in dict, descending sort + convert to tuples,
+        # return top k elems
+
+        element_freqs = {}
+        highest_freqs = []
+
+        for num in nums:
+            element_freqs[num] = element_freqs.get(num, 0) + 1
+        
+        sorted_freqs = sorted(element_freqs.items(), key=lambda x: x[1], reverse=True)
+        for i in range(k):
+            highest_freqs.append(sorted_freqs[i][0])
+
+        return highest_freqs
